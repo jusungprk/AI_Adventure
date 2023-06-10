01a5b7c8e8079ffe25fae2efd1e747c5409e708b
Jusung Park <jusung.park.25@dartmouth.edu>
2023-06-10 17:09:28 -0400

    separated javascript into client-side and server-side

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 require("dotenv").config();
+const fetch = require("node-fetch");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 app.use(cors());
-
-let iterationCount = 0;
-const submitButton = document.querySelector("#generate-btn")
-const inputElement = document.querySelector("#adventure-input")
 
 const getImages = async (prompt) => {
     const options = {
         method: "POST",
         headers: {
-            "Authorization": `Bearer: ${process.env.API_KEY}`,
+            "Authorization": `Bearer ${process.env.API_KEY}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
             prompt: prompt,
             max_tokens: 256
-        })  
+        })
     }
 
     try {
         const response = await fetch('https://api.openai.com/v1/images/generations', options)
         const data = await response.json()
-        console.log(data)
         let imageUrl = data.choices[0].image; // adjust based on actual response structure
-        document.querySelector("#adventure-image").style.backgroundImage = `url(${imageUrl})`;
+        return imageUrl;
     } catch (error) {
         console.error(error)
     }
@@ -38,7 +35,7 @@ const getChoices = async (prompt) => {
     const options = {
         method: "POST",
         headers: {
-            "Authorization": `Bearer: ${API_KEY}`,
+            "Authorization": `Bearer ${process.env.API_KEY}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -51,35 +48,27 @@ const getChoices = async (prompt) => {
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
-        console.log(data)
         let choices = data.choices.map(choice => choice.text); // adjust based on actual response structure
-
-        let choicesElement = document.querySelector("#choices");
-        choicesElement.innerHTML = "";
-
-        for (let choice of choices) {
-            let choiceElement = document.createElement("div");
-            choiceElement.classList.add("choice");
-            choiceElement.innerText = choice;
-            choiceElement.addEventListener("click", () => {
-                if (iterationCount < 10) {
-                    iterationCount++;
-                    processAdventure(choice);
-                }
-            });
-            choicesElement.appendChild(choiceElement);
-        }
+        return choices;
     } catch (error) {
         console.error(error)
     }
 }
 
-const processAdventure = async (prompt) => {
-    await getChoices(prompt);
-    await getImages(prompt);
-}
+app.post("/startAdventure", async (req, res) => {
+    const {prompt} = req.body;
 
-submitButton.addEventListener("click", () => {
-    let initialPrompt = inputElement.value;
-    processAdventure(initialPrompt);
+    // Call your getChoices and getImages functions here
+    const imageUrl = await getImages(prompt);
+    const choices = await getChoices(prompt);
+
+    // Once you have the data, send it back in the response
+    res.json({
+        imageUrl,
+        choices
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
 });
